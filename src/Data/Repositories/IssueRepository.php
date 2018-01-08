1af2b22cfc6035c1dfb617eb2b2ab99db503b201
Marcio Nobrega <marcio.nobrega@premium-minds.com>
2018-01-08 01:22:42 +0000

    WIP - Created command for cronjob. Connects to JIRA, but now needs some testing.

---
+++ src/Data/Repositories/IssueRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mnobrega
+ * Date: 08/01/2018
+ * Time: 01:00
+ */
+
+namespace App\Data\Repositories;
+
+
+class IssueRepository extends Repository
+{
+
+}
--- src/Services/JiraWrapper/Features/CopyJiraIssuesToDatabaseFeature.php
+++ src/Services/JiraWrapper/Features/CopyJiraIssuesToDatabaseFeature.php
@@ -1,13 +1,27 @@
 <?php
 namespace App\Services\JiraWrapper\Features;
 
+use App\Domains\JiraClient\Jobs\GetConnectionJob;
+use App\Domains\JiraClient\Jobs\SearchIssuesByJQLJob;
 use Lucid\Foundation\Feature;
-use Illuminate\Http\Request;
 
 class CopyJiraIssuesToDatabaseFeature extends Feature
 {
-    public function handle(Request $request)
+    public function handle()
     {
+        $getConnectionJobParams = [
+            'host'=>env('JIRA_HOST'),
+            'user'=>env('JIRA_USERNAME'),
+            'pass'=>env('JIRA_PASSWORD'),
+        ];
+        $jiraApi = $this->run(GetConnectionJob::class, $getConnectionJobParams);
 
+        $searchIssuesByJQLParams = [
+            'jiraApi'=>$jiraApi,
+            'query'=>env('JIRA_ISSUES_QUERY'),
+        ];
+        $jiraIssues = $this->run(SearchIssuesByJQLJob::class, $searchIssuesByJQLParams);
+
+        dd($jiraIssues);
     }
 }
