d92b236b8d50ec3511fb2103916b13a85dd02a9c
Márcio Nóbrega <marcio.nobrega@sapo.pt>
2018-03-29 00:40:30 +0100

    For now will stop making this feature, but will merge it with master.

--- src/Services/JiraWrapper/resources/views/welcome.blade.php
+++ src/Services/JiraWrapper/resources/views/welcome.blade.php
@@ -6,8 +6,9 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Jira Wrapper Status</div>
-                        {{dd($featureResult)}}
                     <div class="panel-body">
+                        <span>Número de Issues Sincronizadas: {{$featureResult['issuesCount']}}</span><br />
+                        <span>Última Issue Sincronizada: {{$featureResult['latestUpdatedIssue'][0]->key}}</span>
                     </div>
                 </div>
             </div>
