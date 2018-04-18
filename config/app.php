d27a3bd559876d6499258b97c071b36ee1bce625
Márcio Nóbrega <marcio.nobrega@sapo.pt>
2018-04-18 02:09:24 +0100

    Added missing applicatio config.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Jira Services',
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
